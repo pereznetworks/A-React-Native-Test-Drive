cc5e3f384cd9db31241c6342186f736a68917d1d
pereznetworks <34382720+pereznetworks@users.noreply.github.com>
2024-11-27 18:39:57 -0800

    applied fix, per new react rules
    applying fix per react rules,
    Side effects must run outside of render
    see reenimated docs:
    
    https://docs.swmansion.com/react-native-reanimated/docs/core/useSharedValue#remarks

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -100,4 +100,4 @@ const styles = StyleSheet.create({
   <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
   <ThemedText type="defaultSemiBold">app-example</ThemedText>.
 </ThemedText>
-</ThemedView> */}
+</ThemedView> */
--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -1,4 +1,5 @@
 
+import React from 'react';
 import { StyleSheet } from 'react-native';
 import Animated, {
   useSharedValue,
@@ -10,28 +11,33 @@ import Animated, {
 
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useEffect, useRef } from 'react';
 
 export function HelloWave() {
 
-  let rotationAnimation = useSharedValue(0);
+ let rotationAnimation = useSharedValue(0);
+  // let rotationAnimation = useRef(0);
+
+  React.useEffect(() => {
+    rotationAnimation.value = withRepeat(
+      withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
+      4 // Run the animation 4 times
+      );
+  })
+
+  let animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${rotationAnimation.value}deg` }],
+  }));
 
   const wavAgain  = () => {
     
-    rotationAnimation.value = withRepeat(
-    withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
-    4 // Run the animation 4 times
-    );
-    
-    }
-
-  rotationAnimation.value = withRepeat(
-    withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
-    4 // Run the animation 4 times
-  );
-
-  const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ rotate: `${rotationAnimation.value}deg` }],
-  }));
+    React.useEffect(() => {
+      rotationAnimation.value = withRepeat(
+        withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
+        4 // Run the animation 4 times
+        );
+    })
+  }
 
   return (
       <ThemedView >
